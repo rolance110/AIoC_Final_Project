9b5a9032e17345721ba7433b16eea510ff5ef1e9
0522weixiang <n26131724@gs.ncku.edu.tw>
2025-05-30 17:16:43 +0800

    refactor:原先輸入是32*8bit，現在改成一次輸入32bit，總共需要32個cycle才能填滿所有FIFO

--- src/Horizontal_Buffer.sv
+++ src/Horizontal_Buffer.sv
@@ -4,33 +4,64 @@
 module Horizontal_Buffer(
     input clk,
     input reset,
-    input [`ROW_NUM - 1 : 0] weight_en,
-    input [`ROW_NUM*`COL_NUM*8 - 1 : 0] weight_in,
+    input store_weight_f,
+    input [4:0] weight_en,
+    input [`ROW_NUM - 1 : 0] weight_in,//改成32bit，一個ROW weight需要8個cycle才能填滿
 
-    output logic [`ROW_NUM*`COL_NUM*8 - 1 : 0] weight_out
+    output [`ROW_NUM*`COL_NUM*8 - 1 : 0] weight_out
 );
-  // --------------------------------------------------
-  // TODO 一次會更新同一個ROW的所有COL weights，根據enable來看哪幾個ROW需要被更新
-  // --------------------------------------------------
-  // storage for each PE’s weight
-  logic [7:0] weight_mem [0:`ROW_NUM-1][0:`COL_NUM-1];
+// --------------------------------------------------
+// TODO:
+// 因為每次輸入32bit，一個ROW的weight需要8個cycle才能完成
+// weight_en會說明有幾個ROW被放weight，依照外面FSM被切換的時間(不一定32ROW被填滿)
+// --------------------------------------------------
+// storage for each PE’s weight
+logic [31:0] weight_mem [0:`ROW_NUM-1][0:7];
+logic [2:0] col_cnt;
+logic [4:0] row_cnt;
 
-  genvar r, c;
-  generate
-    for (r = 0; r < `ROW_NUM; r = r + 1) begin : ROW_LOOP
-      for (c = 0; c < `COL_NUM; c = c + 1) begin : COL_LOOP
-        // fully-unrolled always_ff: one for each (r,c)
-        always_ff @(posedge clk or posedge reset) begin
-          if (reset) 
-            weight_mem[r][c] <= 8'd0;
-          else if (weight_en[r])
-            weight_mem[r][c] <= weight_in[((r*`COL_NUM + c)*8) +: 8];
-        end
-
-        // flatten out
-        assign weight_out[((r*`COL_NUM + c)*8) +: 8] = weight_mem[r][c];
-      end
+always_ff @(posedge clk or negedge reset) begin
+    if(reset) begin
+       col_cnt <= 3'd0;
+       row_cnt <= 5'd0;
     end
-  endgenerate
+    else if(store_weight_f) begin
+        if(col_cnt == 3'd7) begin
+            col_cnt <= 3'd0;
+            if(row_cnt == weight_en)//數量相同就可以歸零
+                row_cnt <= 5'd0;
+            else
+                row_cnt <= row_cnt + 5'd1;//不怕她爆掉，就給他在else的時候清0即可
+        end
+        else
+            col_cnt <= col_cnt + 3'd1;
+    end
+    else begin
+        row_cnt <= 5'd0;
+        col_cnt <= 3'd0;
+    end
+end
+
+always_ff @(posedge clk or negedge reset) begin
+   if(reset) begin
+     for(int i=0;i<32;i=i+1) begin
+       for(int j=0;j<8;j=j+1) begin
+         weight_mem[i][j] <= 32'd0;
+       end
+      end
+   end
+   else if(store_weight_f) begin
+      weight_mem[row_cnt][col_cnt] <= weight_in;
+   end
+end
+
+genvar r, c;
+generate
+    for(r=0;r<`ROW_NUM;r=r+1) begin : ROWS
+        for(c=0;c<8;c=c+1) begin : COLS
+            assign weight_out[(r*256)+(c*32) +: 32] = weight_mem[r][c];
+        end
+    end
+endgenerate
 
 endmodule
