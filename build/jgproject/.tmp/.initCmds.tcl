ee752076be2a672637a555d0ceee9ffe513550e4
ROLOLO <n26130126@gs.ncku.edu.tw>
2025-06-15 22:34:06 +0800

    fix: superlint token engine

--- build/jgproject/.tmp/.initCmds.tcl
+++
@@ -1,4 +0,0 @@
-@puts {INFO: successfully checked out licenses "jasper_interactive" and "jasper_dao".}
-@puts {INFO: reading configuration file "/home/n26130126/.config/cadence/jasper.conf".}
-@get_message -clear
-check_superlint -init
