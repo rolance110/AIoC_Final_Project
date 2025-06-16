3859b85847b5a5c788fd6bd4a611a3d60debe504
ROLOLO <n26130126@gs.ncku.edu.tw>
2025-06-16 23:21:10 +0800

    update: gitignore

--- wave/nWaveLog/nWave.cmd
+++
@@ -1,5 +0,0 @@
-wvSetPosition -win $_nWave1 {("G1" 0)}
-wvOpenFile -win $_nWave1 \
-           {/home/n26130126/Desktop/AOC_Final_Project/AIoC_Final_Project/wave/top.fsdb}
-verdiSetActWin -win $_nWave1
-wvExit
